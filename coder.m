23bd69e95f4d823f12b74ebbdeb009c34aff2644
Jimenez <daniel.est.jimenez-leon@hpe.com>
2018-10-22 21:40:12 -0600

    Fixes for encoder

--- coder.m
+++ coder.m
@@ -9,12 +9,12 @@ addpath('./export_fig/');
 %--------------------------------------------------------------------------
 %Input characteristics:
 % 16 bit per sample, at 44100 Hz, in stereo
-[sampleData, sampleFrequency] = audioread('./sample.wav');
+[sampleData, sampleFrequency] = audioread('./sample2.wav');
 
 %--------------------------------------------------------------------------
 % Segment signal
 %--------------------------------------------------------------------------
-segmentSize = 400; %This should come from the parameters.
+segmentSize = 10000; %This should come from the parameters.
 Channel = 1; %This should come from the parameters.
 
 signalSize = size(sampleData(:,Channel));
@@ -33,20 +33,16 @@ for i = 1:segmentTotal
     end
 end
 
-prueba1 = abs(ifft(log(fft(v(10,:))).^2));
-figure(1);
-plot(prueba1);
-%axis([0 400 0 60]);
-hold on;
 %--------------------------------------------------------------------------
-% Multiplexing and Combining
+% Multiplexing
 %--------------------------------------------------------------------------
 metaData = round(rand(segmentTotal,1)); %This should be real metaData.
-metaData(10) = 1;
+t = 10;
+metaData(t) = 1;
 
 %H0(z) = 1 + a0*z^(-t0)
-a0 = 1*10^20; %This could come from the parameters.
-t0 = 100; %This could come from the parameters.
+a0 = 8000; %This could come from the parameters.
+t0 = 2000; %This could come from the parameters.
 
 H0 = zeros(t0,1);
 H0(1) = 1;
@@ -55,8 +51,8 @@ h0 = impz(H0,1);
 h0 = h0';
 
 %H1(z) = 1 + a1*z^(-t1)
-a1 = 5*10^20; %This could come from the parameters.
-t1 = 300; %This could come from the parameters.
+a1 = 2000; %This could come from the parameters.
+t1 = 8000; %This could come from the parameters.
 
 H1 = zeros(t1,1);
 H1(1) = 1;
@@ -64,7 +60,7 @@ H1(t1) = a1;
 h1 = impz(H1,1);
 h1 = h1';
 
-%figure;
+%figure(2);
 %stem(h1);
 %hold on
 %stem(h0);
@@ -75,40 +71,51 @@ temp1 = zeros(1,segmentSize+t1-1);
 ex0 = zeros(1,t0-1);
 ex1 = zeros(1,t1-1);
 
+tMax = t1;
+if (t0>t1)
+    tMax = t0;
+end
+
+temp_y = zeros(segmentTotal,segmentSize+tMax-1);
+
 for i = 1:segmentTotal
     if(metaData(i))
         temp1 = conv(v(i,:),h1);
-        for j = 1:segmentSize+t1-1
-            if (j<=segmentSize)
-                if (j>t1-1)
-                    v(i,j) = temp1(j);
-                else
-                    v(i,j) = temp1(j)+ex1(j);
-                end
-            else
-                ex1(j-segmentSize) = temp1(j);
-            end
+        for j = 2:segmentSize+t1-2
+            temp_y(i,j) = temp1(j);
         end
     else
         temp0 = conv(v(i,:),h0);
-        for j = 1:segmentSize+t0-1
-            if (j<=segmentSize)
-                if (j>t0-1)
-                    v(i,j) = temp0(j);
-                else
-                    v(i,j) = temp0(j)+ex0(j);
-                end
-            else
-                ex0(j-segmentSize) = temp0(j);
-            end
+        for j = 2:segmentSize+t0-2
+            temp_y(i,j) = temp0(j);
         end
     end
 end
 
-prueba2 = abs(ifft(log(fft(v(10,:))).^2));
-plot(prueba2);
-axis([25 375 0 30]);
-hold on;
+%--------------------------------------------------------------------------
+% Combining
+%--------------------------------------------------------------------------
+y = zeros(segmentTotal,segmentSize);
+ex = zeros(segmentTotal,tMax-1);
+
+for i = 1:segmentTotal
+    for j = 1:segmentSize+tMax-1
+        if (j<=segmentSize)
+            if (j>tMax-1)
+                y(i,j) = temp_y(i,j);
+            else
+                y(i,j) = temp_y(i,j)+ex(i,j);
+            end
+        else
+            ex(i,j-segmentSize) = temp_y(i,j);
+        end
+    end
+end
+
+prueba = abs(ifft(log(fft(temp_y(t,:))).^2));
+figure(1);
+plot(prueba);
+axis([0 segmentSize 0 6]);
 
 %--------------------------------------------------------------------------
 % Unifying segments
